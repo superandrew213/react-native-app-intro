5357cfd71d718c410a73436e648746e5a53e5b4f
Andrew Shini <superandrew213@users.noreply.github.com>
2017-05-19 16:53:45 +1000

    Update DoneButton.ios.js

--- components/DoneButton.ios.js
+++ components/DoneButton.ios.js
@@ -10,7 +10,8 @@ export const DoneButton = ({
   styles, onDoneBtnClick, onNextBtnClick,
   rightTextColor, isDoneBtnShow,
   doneBtnLabel, nextBtnLabel,
-  doneFadeOpacity, skipFadeOpacity, nextOpacity
+  doneFadeOpacity, skipFadeOpacity, nextOpacity,
+  textStyle,
 }) => {
   return (
     <View style={styles.btnContainer}>
@@ -27,7 +28,7 @@ export const DoneButton = ({
         <View style={styles.full}>
           <Text style={[styles.controllText, {
             color: rightTextColor, paddingRight: 30,
-          }]}>
+          }, textStyle]}>
             {doneBtnLabel}
           </Text>
         </View>
@@ -35,7 +36,7 @@ export const DoneButton = ({
       <Animated.View style={[styles.full, { height: 0 }, { opacity: nextOpacity }]}>
         <TouchableOpacity style={styles.full}
           onPress={ isDoneBtnShow ? onDoneBtnClick : onNextBtnClick}>
-         <Text style={[styles.nextButtonText, { color: rightTextColor }]}>
+         <Text style={[styles.nextButtonText, { color: rightTextColor }, textStyle]}>
           {nextBtnLabel}
         </Text>
         </TouchableOpacity>
