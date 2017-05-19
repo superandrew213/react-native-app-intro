86fbca750ea8ed4e275d79159e22bfda22dfb922
Andrew Shini <superandrew213@users.noreply.github.com>
2017-05-19 16:55:24 +1000

    Update SkipButton.ios.js

--- components/SkipButton.ios.js
+++ components/SkipButton.ios.js
@@ -10,7 +10,8 @@ export const SkipButton = ({
   styles, onSkipBtnClick, isSkipBtnShow,
   leftTextColor,
   skipBtnLabel,
-  skipFadeOpacity
+  skipFadeOpacity,
+  textStyle
 }) => {
   return (
     <Animated.View style={[styles.btnContainer, {
@@ -26,7 +27,7 @@ export const SkipButton = ({
       <TouchableOpacity
         style={styles.full}
         onPress={isSkipBtnShow ? () => onSkipBtnClick() : null}>
-        <Text style={[styles.controllText, { color: leftTextColor }]}>
+        <Text style={[styles.controllText, { color: leftTextColor }, textStyle]}>
           {skipBtnLabel}
         </Text>
       </TouchableOpacity>
